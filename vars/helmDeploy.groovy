e79832fe26511e57352ff0d2840f22ae2de0a674
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2026-02-28 16:01:00 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -1,45 +1,45 @@
 def call(Map config) {
-    def releaseName = config.releaseName
-    def chartPath = config.chartPath ?: '.'
-    def namespace = config.namespace
-    def setValues = config.setValues ?: []
+    def releaseName      = config.releaseName
+    def chartPath        = config.chartPath ?: '.'
+    def namespace        = config.namespace
+    def setValues        = config.setValues ?: []
     def awsCredentialsId = config.awsCredentialsId
-    def clusterName = config.clusterName
-    def region = config.region ?: 'us-east-1'
+    def clusterName      = config.clusterName
+    def region           = config.region ?: 'us-east-1'
 
     if (!releaseName || !namespace || !clusterName || !awsCredentialsId) {
         error "releaseName, namespace, clusterName, awsCredentialsId required for EKS"
     }
 
-       // EKS auth
-         withAWS(credentials: awsCredentialsId, region: region) {
-                    sh """
-                    mkdir -p "$PWD/.kube"
-                    export KUBECONFIG="$PWD/.kube/config"
-                    aws eks update-kubeconfig --name ${clusterName} --region ${region}
-                    """
-                }
+    // EKS auth
+    withAWS(credentials: awsCredentialsId, region: region) {
+        sh """
+          mkdir -p "\$PWD/.kube"
+          export KUBECONFIG="\$PWD/.kube/config"
+          aws eks update-kubeconfig --name ${clusterName} --region ${region}
+        """
+    }
 
-            def setArgs = setValues.collect { "--set ${it}" }.join(' ')
-            
-            sh """
-                export KUBECONFIG=\$PWD/.kube/config
-                helm lint ${chartPath}
-                """
-            
-            def setArgs = setValues.collect { "--set ${it}" }.join(' ')
-            sh """
-                export KUBECONFIG=\$PWD/.kube/config
-                helm upgrade --install ${releaseName} ${chartPath} \
-                --namespace ${namespace} \
-                --create-namespace \
-                ${setArgs} \
-                --atomic \
-                --timeout 5m \
-                --wait \
-                --history-max 5
-            """
-            sh """
+    def setArgs = setValues.collect { "--set ${it}" }.join(' ')
+
+    sh """
+      export KUBECONFIG=\$PWD/.kube/config
+      helm lint ${chartPath}
+    """
+
+    sh """
+      export KUBECONFIG=\$PWD/.kube/config
+      helm upgrade --install ${releaseName} ${chartPath} \
+        --namespace ${namespace} \
+        --create-namespace \
+        ${setArgs} \
+        --atomic \
+        --timeout 5m \
+        --wait \
+        --history-max 5
+    """
+
+    sh """
       export KUBECONFIG=\$PWD/.kube/config
       helm status ${releaseName} -n ${namespace}
     """
