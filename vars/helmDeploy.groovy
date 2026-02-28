9e555c1fe903292e6cd293c9719e1b3fb344e050
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2026-02-28 10:46:04 +0530

    Create helmDeploy.groovy

---
+++ vars/helmDeploy.groovy
@@ -0,0 +1,39 @@
+def call(Map config) {
+    def releaseName = config.releaseName
+    def chartPath = config.chartPath ?: '.'
+    def namespace = config.namespace
+    def setValues = config.setValues ?: []
+    def awsCredentialsId = config.awsCredentialsId
+    def clusterName = config.clusterName
+    def region = config.region ?: 'us-east-1'
+
+    if (!releaseName || !namespace || !clusterName || !awsCredentialsId) {
+        error "releaseName, namespace, clusterName, awsCredentialsId required for EKS"
+    }
+
+    node {
+        container('helm') {
+            // EKS auth
+           (awsCredentialsId) {
+                withAWS(credentials: awsCredentialsId, region: region) {
+                    sh "aws eks update-kubeconfig --name ${clusterName} --region ${region}"
+                }
+            }
+
+            sh "helm lint ${chartPath}"
+            
+            def setArgs = setValues.collect { "--set ${it}" }.join(' ')
+            sh """
+                helm upgrade --install ${releaseName} ${chartPath} \
+                --namespace ${namespace} \
+                --create-namespace \
+                ${setArgs} \
+                --atomic \
+                --timeout 5m \
+                --wait \
+                --history-max 5
+            """
+            sh "helm status ${releaseName} -n ${namespace}"
+        }
+    }
+}
