ef7da03c485810ea5960896051ffd89202eb1b0f
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2026-03-05 13:11:34 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -10,12 +10,11 @@ def call(Map config) {
     if (!releaseName || !namespace || !clusterName || !awsCredentialsId) {
         error "releaseName, namespace, clusterName, awsCredentialsId required for EKS"
     }
-
     // EKS auth
-    withAWS(credentials: awsCredentialsId, region: region) {
-        sh """
-         aws eks update-kubeconfig --name ${clusterName} --region ${region}
-        """
+    //withAWS(credentials: awsCredentialsId, region: region) {
+    //    sh """
+    //     aws eks update-kubeconfig --name ${clusterName} --region ${region}
+    //    """
     def setArgs = setValues.collect { "--set ${it}" }.join(' ')
 
     sh """
